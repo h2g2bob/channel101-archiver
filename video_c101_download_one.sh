da8fd2c5cc70bb5afd798a9547c8ab1d287837a0
David Batley <git@dbatley.com>
2019-01-07 23:18:27 +0000

    Download one video at a time
    Lets us put this in a cron job and eventually it'll be done
    (without overloading the website)

--- video_c101_download_one.sh
+++ video_c101_download_one.sh
@@ -2,7 +2,11 @@
 destdir="data/video_c101"
 mkdir -p "$destdir"
 grep -h -E -o 'http://www.channel101.com/s/videos/[a-z0-9]+\.m4v' data/episode_description/*.html \
+| sort --random-sort \
 | while read url; do
 	dest="$destdir/${url##*/}"
-	[ -e "$dest" ] || wget -O "$dest" "$url"
+	[ -e "$dest" ] || {
+		wget -O "$dest" "$url"
+		exit 0
+	}
 done
