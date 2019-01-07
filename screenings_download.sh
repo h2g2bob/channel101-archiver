c3f2ae777ce888895318d57a7e8dd946b3f29ad0
David Batley <git@dbatley.com>
2019-01-07 22:50:20 +0000

    Make the directory when needed

--- screenings_download.sh
+++ screenings_download.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+destdir="data/screenings"
+mkdir -p "$destdir"
 for ((num=1; num <= 245; num++)); do
-	out="data/screenings/${num}.html"
+	out="$destdir/${num}.html"
 	[ -e "$out" ] || wget -O "$out" "http://www.channel101.com/shows-content/$num"
 done
