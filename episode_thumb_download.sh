1fd42659da8c8a4146aa4ccec799ac48628f71c0
David Batley <git@dbatley.com>
2019-01-16 16:04:04 +0000

    Remove old scripts
    The useful code is merged into the download/archive scripts.

--- episode_thumb_download.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-destdir="data/episode_thumb"
-mkdir -p "$destdir"
-grep -h -E -o 'http://www.channel101.com/image-cache/200/113/95/images/episode/[a-z0-9]+\.jpg' data/screenings/*.html \
-| while read url; do
-	dest="$destdir/${url##*/}"
-	[ -e "$dest" ] || wget -O "$dest" "$url"
-done
